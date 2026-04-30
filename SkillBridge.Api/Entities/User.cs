e79cd43408f024f2ad9a18cee92f20a37c6f1c24
Amit Kushwaha <ar.amitnp@gmail.com>
2026-04-30 08:12:53 +0545

    user entitity class added

---
+++ SkillBridge.Api/Entities/User.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+public class User
+{
+    [Required, Key]
+    public int Id {get;set;}
+    [Required]
+    public string Name {get; set;}
+    [Required, EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    public string PasswordHash { get; set; }
+    [Required]
+    public string Type { get; set; }
+    [Required]
+    public DateTime CreatedAt { get; set; }
+    
+    public bool IsActive { get; get; }
+    }
