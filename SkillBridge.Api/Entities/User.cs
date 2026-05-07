d2c1b80f3f913ccc5481a39d541090dd0b3a594b
Amit Kushwaha <ar.amitnp@gmail.com>
2026-05-07 08:30:34 +0545

    Upadte 2

--- SkillBridge.Api/Entities/User.cs
+++ SkillBridge.Api/Entities/User.cs
@@ -1,21 +1,27 @@
+using System;
 using System.ComponentModel.DataAnnotations;
-using System.Security.Cryptography.X509Certificates;
-{
+
 public class User
 {
-    [Required, Key]
-    public int Id {get;set;}
     [Required]
-    public string Name {get; set;}
-    [Required, EmailAddress]
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
+
     [Required]
     public string PasswordHash { get; set; }
+
     [Required]
     public string Type { get; set; }
+
     [Required]
     public DateTime CreatedAt { get; set; }
-    
-    public bool IsActive { get; get; }
-    }
+
+    public bool IsActive { get; set; }
 }
--- SkillBridge.Api/SkillBridgeDbContext.cs
+++ SkillBridge.Api/SkillBridgeDbContext.cs
@@ -1,1 +1,15 @@
- 
+using SkillBridge.Api.Entities;
+
+public class SkillBridgeDbContext : DbContext
+{
+    public SkillBridgeDbContext(
+        DbContextOptions<SkillBridgeDbContext> options
+    ) : base(options)
+    {
+    }
+
+    // Define your DbSets here, for example:
+    // public DbSet<User> Users { get; set; }
+
+    public DbSet<User> Users { get; set; }
+}
